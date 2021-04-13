e96f363c58fe81756d8ec999e89c78d84062e3c8
Elias Kohout <elias@kohout.de>
2021-04-13 19:24:47 +0200

    Erweitern der Klasse SPIELER um die Methoden guthabenAendern und nameAendern.

--- SPIELER.py
+++ SPIELER.py
@@ -2,9 +2,26 @@
 SPIELER.py -> Ein Klasse, die Informationen und Methoden bezüglich des Spielers enthält.
 """
 
+import time
+
 
 class SPIELER:
 
-    def __init__(self, name, startguthaben: float = 0.0):  # Konstruktor
+    def __init__(self, name, startguthaben = 0.0):  # Konstruktor
         self.name = name
-        self.guthaben = startguthaben
+        self.guthaben = 0
+        self.guthabenHistorie = []
+        self.guthabenAendern(startguthaben)
+
+    def guthabenAendern (self, betrag: int):
+        self.guthaben += betrag
+        self.guthabenHistorie.append({
+            'Datum': time.strftime("%d %m %y"),
+            'Uhrzeit': time.strftime("%H %M %S"),
+            'Guthaben': self.guthaben
+        })
+
+    def nameAendern (self, neuerName: str):
+        self.name = neuerName
+
+    
