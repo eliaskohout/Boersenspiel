4daa2d154e0d3b99cc1f0091cedc2fc18f30dddb
Elias Kohout <elias@kohout.de>
2021-04-27 17:40:51 +0200

    Implementierung des Haupt-Fensters, das beim Ausführen erzeugt wird.

--- main.py
+++ main.py
@@ -2,18 +2,42 @@
 main.py -> Die main Funktion des Börsenspiels.
 """
 
-import SPIELER as S
-import UI
-import MARKETSTACK as M
+import source.SPIELER as S
+import source.MARKETSTACK as M
+
+from ui.main_window import Ui_Form
+
+from PyQt5 import QtWidgets as qtw
+from PyQt5 import QtCore as qtc
 
 
 def main():
-    spieler = S.SPIELER('Elias')
-    ui = UI.UI()
+    spieler = S.SPIELER('Bob')
     api = M.MARKETSTACK()
 
-    print('Fertig.')
+
+class MainWindow(qtw.QWidget):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.ui = Ui_Form()
+        self.ui.setupUi(self)
+
+        # Hier die können die Methoden mit den Widgets verbunden werden
+        self.ui.pushButton.clicked.connect(self.drucke)
+
+    # Hier die Methoden für Funktionen der Widgets (z.B. Button) einfügen
+    def drucke(self):
+        print(self.ui.textEdit.toPlainText())
 
 
 if __name__ == "__main__":
     main()
+
+    app = qtw.QApplication([])
+
+    widget = MainWindow()
+    widget.show()
+
+    app.exec_()
