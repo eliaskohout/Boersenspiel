c4e49e1dc91074a1cd97b44085da1fe2a678d630
Elias Kohout <elias@kohout.de>
2021-05-19 00:09:07 +0200

    Hinzufügen der Auswahl von gesuchten Wertpapieren.

--- main.py
+++ main.py
@@ -20,23 +20,36 @@ class MainWindow(qtw.QWidget):
         super().__init__(*args, **kwargs)
 
         self.daten = D.DATEN()
-        self.daten.tickerErneuern()
         self.daten.tickers.saveToFile()
 
         self.ui = Ui_Form()
         self.ui.setupUi(self)
 
-        # Hier können die Methoden mit den Widgets verbunden werden
+        self.ui.tabWidget.setTabVisible(3, False)
+
+        # Hier können die Methoden mit den Signalen der Widgets verbunden werden
 
         self.ui.pushButton_aktiensuche.clicked.connect(self.suche)
+        self.ui.listWidget_suchergebnis.itemDoubleClicked.connect(self.launchAktieninfo)
 
     # Hier die Methoden für Funktionen der Widgets (z.B. Button) einfügen
 
-    def suche(self):
+    def suche( self ):
         self.ui.listWidget_suchergebnis.clear()
         phrase = self.ui.plainTextEdit_aktiensuche.toPlainText()
         liste = ["{} ({})".format(e[0], e[1]) for e in self.daten.tickers.inhaltSuchen(phrase)]
         self.ui.listWidget_suchergebnis.addItems(liste)
+
+    def launchAktieninfo( self, qListItem ):
+        label = qListItem.text()
+        ticker = label.split('(')[1][:-1]
+        self.ui.tabWidget.setTabText(3, label)
+        self.ui.tabWidget.setTabVisible(3, True)
+        self.ui.tabWidget.setCurrentIndex(3)
+        self.konfiguriereAktieninfo(ticker)
+
+    def konfiguriereAktieninfo( self, ticker: str ):
+        pass
 
 
 if __name__ == "__main__":
