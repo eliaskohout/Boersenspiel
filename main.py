841e936b15348864573b792b6063b9c0c34ccef8
elias70 <55064653+elias70@users.noreply.github.com>
2021-06-10 20:38:15 +0200

    Implementierung von threading (experimentell)

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from ui.main_window import Ui_Form
 
 from PyQt5 import QtWidgets as qtw
 from PyQt5 import QtCore as qtc
+import threading
 
 
 def main():
@@ -27,7 +28,7 @@ class MainWindow(qtw.QWidget):
         self.ui = Ui_Form()
         self.ui.setupUi(self)
         self.ui.tabWidget.setTabVisible(3, False)
-        self.aktualisiereTabProtfolio()
+        self.aktualisiereTabPortfolio()
 
         #self.daten.tickerErneuern()
         #self.daten.tickerbaum.saveToFile()
@@ -38,27 +39,33 @@ class MainWindow(qtw.QWidget):
         self.ui.listWidget_suchergebnis.itemDoubleClicked.connect(self.launchAktieninfo)
         self.ui.listWidget_gekaufteAktien.itemDoubleClicked.connect(self.launchAktieninfo)
         self.ui.pushButton_kaufen.clicked.connect(self.kaufenclick)
-        self.ui.pushButton_kaufen.clicked.connect(self.aktualisiereAktienanzahlLabel)
+        self.ui.pushButton_kaufen.clicked.connect(self.aktualisiereImBesitzLabel)
         self.ui.pushButton_verkaufen.clicked.connect(self.verkaufenclick)
-        self.ui.pushButton_verkaufen.clicked.connect(self.aktualisiereAktienanzahlLabel)
-        self.ui.tabWidget.currentChanged.connect(self.aktualisiereTabProtfolio)
+        self.ui.pushButton_verkaufen.clicked.connect(self.aktualisiereImBesitzLabel)
+        self.ui.tabWidget.currentChanged.connect(self.aktualisiereTabPortfolio)
         self.ui.pushButton_preis.clicked.connect(self.aktualisierePreisLabel)
 
     # Hier die Methoden für Funktionen der Widgets (z.B. Button) einfügen
 
     def kaufenclick( self ):
-        self.spieler.wertpapierKaufen(int(str(self.ui.spinBox_anzahlKaufen.value())), self.aktuellerTicker)
+        self.imHintergrundAusführen( self.spieler.wertpapierKaufen, (int(self.ui.spinBox_anzahlKaufen.value()), self.aktuellerTicker) )
 
     def verkaufenclick( self ):
-        self.spieler.wertpapierVerkaufen(int(str(self.ui.spinBox_anzahlVerkaufen.value())), self.aktuellerTicker)
+        self.imHintergrundAusführen( self.spieler.wertpapierVerkaufen, (int(self.ui.spinBox_anzahlVerkaufen.value()), self.aktuellerTicker) )
 
-    def aktualisierePreisLabel( self ):
+    def aktualisierePreisLabel( self, threaded=True ):
+        if threaded:
+            self.imHintergrundAusführen( self.aktualisierePreisLabel, (False))
+            return
         tickerpreis = self.daten.aktuellenTickerpreisErhalten(self.aktuellerTicker)
         aktiensumme = self.ui.spinBox_anzahlKaufen.value() - self.ui.spinBox_anzahlVerkaufen.value()
         tickerpreis *= aktiensumme
         self.ui.label_preis.setText("%3.2f €" % tickerpreis)
 
-    def aktualisiereAktienanzahlLabel( self ):
+    def aktualisiereImBesitzLabel( self, threaded=True):
+        if threaded:
+            self.imHintergrundAusführen( self.aktualisiereImBesitzLabel, (False))
+            return
         self.ui.label_imBesitz.setText("Im Besitz: %d" % self.spieler.aktienAnzahlErhalten(self.aktuellerTicker))
 
     def suche( self ):
@@ -79,13 +86,20 @@ class MainWindow(qtw.QWidget):
     def konfiguriereAktieninfo( self, ticker: str ):
         self.ui.label_preis.setText("   €")
 
-    def aktualisiereTabProtfolio( self , i =0 ):
+    def aktualisiereTabPortfolio( self , i =0, threaded=True ):
         if i != 0: return
+        if threaded:
+            self.imHintergrundAusführen( self.aktualisiereTabPortfolio, (i, False) )
+            return
         self.ui.listWidget_gekaufteAktien.clear()
         itemlist = ["%s (%s)" % (self.daten.aktiennameErhalten(e), e) for e in self.spieler.aktienliste]
         self.ui.listWidget_gekaufteAktien.addItems(itemlist)
         self.ui.label_depotwert.setText("Depotwert: %3.2f €" % self.spieler.depotwertBerechnen())
         self.ui.label_guthaben.setText( "Guthaben:  %3.2f €" % self.spieler.guthaben)
+
+    def imHintergrundAusführen( self, funktion: list, arguments: tuple):
+        x = threading.Thread(target=funktion, args=arguments)
+        x.start()
 
 
 if __name__ == "__main__":
